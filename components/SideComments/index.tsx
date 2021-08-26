724d6f5c250984bfa3a4264640e04aff77dcf247
kravchuk21 <krauchukvlad@gmail.com>
2021-08-26 14:57:49 +0300

    УроСделал формау авторизации, поправил меню

--- components/SideComments/index.tsx
+++ components/SideComments/index.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import ArrowRightIcon from '@material-ui/icons/NavigateNextOutlined';
+import React from "react";
+import ArrowRightIcon from "@material-ui/icons/NavigateNextOutlined";
 
-import data from '../../data';
-import styles from './SideComments.module.scss';
-import {CommentItem} from "./CommentItem";
+import data from "../../data";
+import styles from "./SideComments.module.scss";
+import { CommentItem } from "./CommentItem";
 import clsx from "clsx";
 
-
 export const SideComments = () => {
-    const [visible, setVisible] = React.useState(true);
+  const [visible, setVisible] = React.useState(true);
 
-    const toggleVisible = () => {
-        setVisible(!visible);
-    }
+  const toggleVisible = () => {
+    setVisible(!visible);
+  };
 
-    return (
+  return (
     <div className={clsx(styles.root, !visible && styles.rotated)}>
       <h3 onClick={toggleVisible}>
         Комментарии <ArrowRightIcon />
       </h3>
-      {visible && data.comments.popular.map((obj) => (
-        <CommentItem key={obj.id} {...obj} />
-      ))}
+      {visible &&
+        data.comments.popular.map((obj) => (
+          <CommentItem key={obj.id} {...obj} />
+        ))}
     </div>
   );
 };
